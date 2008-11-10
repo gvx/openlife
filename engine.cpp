2c85b61e4ff218b0c77160b176f5c5a7c8b462ec
robin User <gyvox.public@gmail.com>
2008-11-10 18:29:10 +0100

    Added engine.cpp...

---
+++ engine.cpp
@@ -0,0 +1,6 @@
+//OpenLife engine
+/* Things it does:
+    * Lets Irrlicht put things on screen
+    * Asks IrrKlang to make some sounds
+    * Uses the Stackless API to run the main script
+    * Nothing yet, hounestly */
